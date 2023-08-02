a1ca9c68af74820b4869c291cc59f81f73c7f0b9
serjG901 <70537823+serjG901@users.noreply.github.com>
2023-08-03 00:25:30 +0300

    Update Input.tsx

--- src/Input.tsx
+++ src/Input.tsx
@@ -34,7 +34,7 @@ export default function Input({ files, handleAdd }: IInput) {
         type="number"
         ref={inputRef}
         required
-        placeholder="size"
+        placeholder="5 or 9 and etc."
         min="1"
         max="100"
       />
